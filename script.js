653c23bb5e690412fafd907aad86b359e9687b0a
Pooja Raut <pooja.raut264@gmail.com>
2021-07-24 08:54:01 -0500

    bonus

--- script.js
+++ script.js
@@ -2,37 +2,49 @@
 window.addEventListener("load", function(){
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
         response.json().then(function(json) {
-            const container = document.getElementById("container");
-            let index = 0;
-            
-            for(index = 0;index<json.length;index++) {
-                
-                if (json[index].active) {
+
+            let jsonArr = [];
+            let jsonObj = {};
+            let jsonSorted = {};
+
+            for(let index = 0;index<json.length;index++) {
+              jsonObj[json[index].hoursInSpace] = json[index];
+              jsonArr.push(json[index].hoursInSpace);
+            }
+            jsonArr.sort();
+
+            for(let index = 0;index<jsonArr.length;index++) {
+                jsonSorted[index] = jsonObj[jsonArr[jsonArr.length-index-1]];
+            }            
+
+            const container = document.getElementById("container");         
+            for(let index = 0;index<json.length;index++) { 
+                if (jsonSorted[index].active) {
                     container.innerHTML += `
                     <div class="astronaut">
                         <div class="bio">
-                            <h3>${json[index].firstName} ${json[index].lastName}</h3>
+                            <h3>${jsonSorted[index].firstName} ${jsonSorted[index].lastName}</h3>
                             <ul>
-                                <li>Hours in space: ${json[index].hoursInSpace}</li>
-                                <li style="color:green">Active: ${json[index].active}</li>
-                                <li>Skills: ${json[index].skills}</li>
+                                <li>Hours in space: ${jsonSorted[index].hoursInSpace}</li>
+                                <li style="color:green">Active: ${jsonSorted[index].active}</li>
+                                <li>Skills: ${jsonSorted[index].skills}</li>
                             </ul>
                         </div>
-                        <img class="avatar" src=${json[index].picture}>
+                        <img class="avatar" src=${jsonSorted[index].picture}>
                     </div>
                     `;
                 } else {
                     container.innerHTML += `
                     <div class="astronaut">
                         <div class="bio">
-                            <h3>${json[index].firstName} ${json[index].lastName}</h3>
+                            <h3>${jsonSorted[index].firstName} ${jsonSorted[index].lastName}</h3>
                             <ul>
-                                <li>Hours in space: ${json[index].hoursInSpace}</li>
-                                <li>Active: ${json[index].active}</li>
-                                <li>Skills: ${json[index].skills}</li>
+                                <li>Hours in space: ${jsonSorted[index].hoursInSpace}</li>
+                                <li style>Active: ${jsonSorted[index].active}</li>
+                                <li>Skills: ${jsonSorted[index].skills}</li>
                             </ul>
                         </div>
-                        <img class="avatar" src=${json[index].picture}>
+                        <img class="avatar" src=${jsonSorted[index].picture}>
                     </div>
                     `;
                 }
