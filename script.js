6ffe22e0f76b69f95093d0a06e9f2d4ddd3014ac
Pooja Raut <pooja.raut264@gmail.com>
2021-07-24 06:18:40 -0500

    Studio JSON

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 // TODO: add code here
+window.addEventListener("load", function(){
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+        response.json().then(function(json) {
+            const container = document.getElementById("container");
+            let index = 0;
+            
+            for(index = 0;index<json.length;index++) {
+                
+                if (json[index].active) {
+                    container.innerHTML += `
+                    <div class="astronaut">
+                        <div class="bio">
+                            <h3>${json[index].firstName} ${json[index].lastName}</h3>
+                            <ul>
+                                <li>Hours in space: ${json[index].hoursInSpace}</li>
+                                <li style="color:green">Active: ${json[index].active}</li>
+                                <li>Skills: ${json[index].skills}</li>
+                            </ul>
+                        </div>
+                        <img class="avatar" src=${json[index].picture}>
+                    </div>
+                    `;
+                } else {
+                    container.innerHTML += `
+                    <div class="astronaut">
+                        <div class="bio">
+                            <h3>${json[index].firstName} ${json[index].lastName}</h3>
+                            <ul>
+                                <li>Hours in space: ${json[index].hoursInSpace}</li>
+                                <li>Active: ${json[index].active}</li>
+                                <li>Skills: ${json[index].skills}</li>
+                            </ul>
+                        </div>
+                        <img class="avatar" src=${json[index].picture}>
+                    </div>
+                    `;
+                }
+
+            }
+
+        });
+    });
+ });
